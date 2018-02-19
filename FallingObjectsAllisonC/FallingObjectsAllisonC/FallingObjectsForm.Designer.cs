c9f29be4f0c32a731510c2f5dcf004518f386650
Allison <36515870+AllisonCook27@users.noreply.github.com>
2018-02-19 16:28:09 -0500

    Add files via upload

---
+++ FallingObjectsAllisonC/FallingObjectsAllisonC/FallingObjectsForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace FallingObjectsAllisonC
+{
+    partial class frmFallingObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterTime = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblEnterHeight = new System.Windows.Forms.Label();
+            this.lblAnswerText = new System.Windows.Forms.Label();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniQuestion = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniQuestion1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniQuestion2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEnterTime
+            // 
+            this.lblEnterTime.AutoSize = true;
+            this.lblEnterTime.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterTime.Location = new System.Drawing.Point(12, 32);
+            this.lblEnterTime.Name = "lblEnterTime";
+            this.lblEnterTime.Size = new System.Drawing.Size(381, 19);
+            this.lblEnterTime.TabIndex = 0;
+            this.lblEnterTime.Text = "Enter the time (in seconds)  since you released the object:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(291, 183);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 19);
+            this.lblAnswer.TabIndex = 1;
+            // 
+            // lblEnterHeight
+            // 
+            this.lblEnterHeight.AutoSize = true;
+            this.lblEnterHeight.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterHeight.Location = new System.Drawing.Point(13, 65);
+            this.lblEnterHeight.Name = "lblEnterHeight";
+            this.lblEnterHeight.Size = new System.Drawing.Size(0, 19);
+            this.lblEnterHeight.TabIndex = 2;
+            // 
+            // lblAnswerText
+            // 
+            this.lblAnswerText.AutoSize = true;
+            this.lblAnswerText.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerText.Location = new System.Drawing.Point(13, 183);
+            this.lblAnswerText.Name = "lblAnswerText";
+            this.lblAnswerText.Size = new System.Drawing.Size(216, 19);
+            this.lblAnswerText.TabIndex = 3;
+            this.lblAnswerText.Text = "The height above the ground is:";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile,
+            this.mniQuestion});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(515, 24);
+            this.mnuMenu.TabIndex = 4;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(92, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // mniQuestion
+            // 
+            this.mniQuestion.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniQuestion1,
+            this.mniQuestion2});
+            this.mniQuestion.Name = "mniQuestion";
+            this.mniQuestion.Size = new System.Drawing.Size(67, 20);
+            this.mniQuestion.Text = "Question";
+            // 
+            // mniQuestion1
+            // 
+            this.mniQuestion1.Name = "mniQuestion1";
+            this.mniQuestion1.Size = new System.Drawing.Size(131, 22);
+            this.mniQuestion1.Text = "Question 1";
+            this.mniQuestion1.Click += new System.EventHandler(this.mniQuestion1_Click);
+            // 
+            // mniQuestion2
+            // 
+            this.mniQuestion2.Name = "mniQuestion2";
+            this.mniQuestion2.Size = new System.Drawing.Size(131, 22);
+            this.mniQuestion2.Text = "Question 2";
+            this.mniQuestion2.Click += new System.EventHandler(this.mniQuestion2_Click);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(399, 32);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 5;
+            this.txtTime.Text = "Enter a Number";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(399, 66);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 6;
+            this.txtHeight.Text = "Enter a Number";
+            this.txtHeight.Visible = false;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCalculate.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(210, 138);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(81, 32);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmFallingObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 261);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblAnswerText);
+            this.Controls.Add(this.lblEnterHeight);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblEnterTime);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmFallingObjects";
+            this.Text = "Falling Objects by Allison Cook";
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterTime;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblEnterHeight;
+        private System.Windows.Forms.Label lblAnswerText;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ToolStripMenuItem mniQuestion;
+        private System.Windows.Forms.ToolStripMenuItem mniQuestion1;
+        private System.Windows.Forms.ToolStripMenuItem mniQuestion2;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ FallingObjectsAllisonC/FallingObjectsAllisonC/FallingObjectsForm.cs
@@ -0,0 +1,150 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 18 February, 2018
+ * Created for: ICS3U Programming
+ * Assignment 2 - falling Objects
+ * This program takes user input of at what time to measure the height of an object falling from
+ * a 100 meter cliff, as well as a second question where the user inputs the time as well as the
+ * height of the cliff
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectsAllisonC
+{
+    public partial class frmFallingObjects : Form
+    {
+        public frmFallingObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //when Exit is clicked it will close the program
+            this.Close();
+        }
+
+        private void mniQuestion1_Click(object sender, EventArgs e)
+        {
+            //Taking away unneeded information for question 1
+            txtHeight.Visible = false;
+            lblEnterHeight.Text = "";
+        }
+
+        private void mniQuestion2_Click(object sender, EventArgs e)
+        {
+            //displaying the additional information for question 2
+            txtHeight.Visible = true;
+            lblEnterHeight.Text = "Enter the height (in meters) of the cliff:";
+        }
+
+        private void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Checking which question it is by what is shown
+            if (txtHeight.Visible == false)
+            {
+                int Time;
+                try
+                {
+                    Time = int.Parse(txtTime.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Please input a number.");
+                }
+                //checking if the number is posistive
+                if (Time > 0)
+                {
+                    //using formual to find height
+                    double AnswerT = Convert.ToInt32(100 - 0.5 * 9.81 * Math.Pow(Time, 2));
+                    //rounding to two decimal places
+                    AnswerT = Math.Round(AnswerT, 2);
+                    if (AnswerT > 0)
+                    {
+                        // Chaning the text to be the answer
+                        lblAnswer.Text = Convert.ToString(AnswerT);
+                    }
+                    else
+                    {
+                        MessageBox.Show("At that time the object would have already hit the ground.");
+                    }
+
+
+                }
+                //if the number is not positive then display message
+                else
+                {
+                    MessageBox.Show("Please enter a positive number.");
+                }
+
+
+            }
+            //Repeating but with two varriable for the second question
+            else
+            {
+                //trying it stops from crashing the program
+                int Time;
+                try
+                {
+                    Time = int.Parse(txtTime.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Please input a number for time.");
+                }
+                int Height;
+                try
+                {
+                    Height = int.Parse(txtHeight.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Please input a number for height.");
+                }
+
+                //checking if the number is posistive
+                if (Time > 0)
+                {
+                    if (Height > 0)
+                    {
+                         //using formual to find height
+                         double AnswerT = Convert.ToInt32(100 - 0.5 * 9.81 * Math.Pow(Time, 2));
+                         //rounding to two decimal places
+                         AnswerT = Math.Round(AnswerT, 2);
+                        if (AnswerT > 0)
+                        {
+                            // Chaning the text to be the answer
+                            lblAnswer.Text = Convert.ToString(AnswerT);
+                        }
+                        else
+                        {
+                            MessageBox.Show("At that time the object would have already hit the ground.");
+                        }
+                    }
+                }   //if the number is not positive then display message
+                    else
+                    {
+                       MessageBox.Show("Please enter a positive number.");
+                    } 
+                    
+
+
+            }
+        }
+    }
+}
+
+
